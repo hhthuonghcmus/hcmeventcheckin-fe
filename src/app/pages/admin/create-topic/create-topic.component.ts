2966687359218bbd9634ce8f0804efa41d1a4971
Ho Hoang Thuong <hoo6hc@bosch.com>
2025-03-12 15:43:11 +0700

    dd

--- src/app/pages/admin/create-topic/create-topic.component.ts
+++ src/app/pages/admin/create-topic/create-topic.component.ts
@@ -7,31 +7,75 @@ import { QuestionService } from '../../../services/question.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { RadioButton } from 'primeng/radiobutton';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Button, ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { Topic } from '../../../interfaces/topic.interface';
+import { text } from 'stream/consumers';
 
 @Component({
   selector: 'app-create-topic',
-  imports: [ CommonModule, InputText, FloatLabel, Select, RadioButton, ReactiveFormsModule, FormsModule, ButtonModule, RippleModule],
+  imports: [
+    CommonModule,
+    InputText,
+    FloatLabel,
+    Select,
+    RadioButton,
+    ReactiveFormsModule,
+    FormsModule,
+    ButtonModule,
+    RippleModule,
+  ],
   templateUrl: './create-topic.component.html',
   styleUrl: './create-topic.component.scss',
 })
 export class CreateTopicComponent {
+  topicForm: FormGroup;
   questionTypes$: Observable<QuestionType[]>;
   categories: any[] = [
     { name: 'Accounting', key: 'A' },
     { name: 'Marketing', key: 'M' },
     { name: 'Production', key: 'P' },
-    { name: 'Research', key: 'R' }
-];
-selectedCategory: any = null;
+    { name: 'Research', key: 'R' },
+  ];
+  selectedCategory: any = null;
 
-  constructor(private questionService: QuestionService) {
-    this.questionTypes$ = this.questionService.questionTypes$.asObservable();
-  }
+  constructor(
+    private questionService: QuestionService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.questionTypes$ = this.questionService.questionTypes$.asObservable();
     this.questionService.fetchQuestionTypes();
+    this.topicForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      questions: this.formBuilder.array([]),
+    });
+  }
+
+  addQuestion() {
+    const questions = this.topicForm.get('questions') as FormArray;
+    questions.push(this.formBuilder.group({
+      text: ['', Validators.required],
+      answers: this.formBuilder.array([])
+    }));
+    console.log(this.topicForm.value)
+    console.log(questions)
+  }
+
+  createTopic() {}
+
+  back() {}
+
+  onSubmit() {
+
   }
 }
